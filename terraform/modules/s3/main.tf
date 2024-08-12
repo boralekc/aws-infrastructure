f5b90f5955ddbb92927ccd92e04d4d6d7c80aac3
boralekc <boralekc@gmail.com>
2024-08-12 11:37:44 +0200

    6

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,25 +1,28 @@
-provider "aws" {
-  region = var.region
-}
-
+# Определение ресурса бакета S3
 resource "aws_s3_bucket" "s3" {
   bucket = var.bucket_name
-  acl    = "public-read"
-
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
   tags = {
     Name        = var.bucket_name
     Environment = var.environment
+  }
+}
+
+# Определение ресурса версионирования S3
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.s3.bucket
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Определение ресурса для настройки шифрования
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.s3.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
