9d7bc1b4d1dac4b0aad72f1a316bf6f129780419
boralekc <boralekc@gmail.com>
2024-08-13 17:54:38 +0200

    2

--- terraform/modules/kubernetes/main.tf
+++ terraform/modules/kubernetes/main.tf
@@ -88,12 +88,12 @@ resource "aws_iam_role" "eks_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "eks_service_policy" {
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role      = aws_iam_role.eks_role.name
 }
 
-resource "aws_iam_role_policy_attachment" "eks_service_policy" {
+resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
   role      = aws_iam_role.eks_role.name
 }
