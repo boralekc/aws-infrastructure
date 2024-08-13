ffae0b5de309d6d01f6be5935ab773688474136e
boralekc <boralekc@gmail.com>
2024-08-13 18:04:49 +0200

    3

--- terraform/modules/kubernetes/main.tf
+++ terraform/modules/kubernetes/main.tf
@@ -70,7 +70,7 @@ resource "aws_eks_cluster" "k8s_cluster" {
   }
 }
 
-# Роль IAM для EKS
+# Создание роли IAM для EKS
 resource "aws_iam_role" "eks_role" {
   name = "eks-cluster-role"
 
@@ -88,6 +88,7 @@ resource "aws_iam_role" "eks_role" {
   })
 }
 
+# Политики IAM для роли EKS
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role      = aws_iam_role.eks_role.name
@@ -96,29 +97,6 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
 resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
   role      = aws_iam_role.eks_role.name
-}
-
-# Создание группы узлов (Node Group) для EKS
-resource "aws_eks_node_group" "k8s_node_group" {
-  cluster_name    = aws_eks_cluster.k8s_cluster.name
-  node_role_arn   = aws_iam_role.node_role.arn
-  subnet_ids = [
-      aws_subnet.k8s_subnet_a.id,
-      aws_subnet.k8s_subnet_b.id
-    ]
-  scaling_config {
-    desired_size = var.node_desired_size
-    max_size     = var.node_max_size
-    min_size     = var.node_min_size
-  }
-
-  instance_types = ["t3.medium"] # Укажите тип инстанса EC2 для узлов
-
-  depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
-  ]
 }
 
 # Создание роли IAM для узлов EKS
@@ -132,18 +110,14 @@ resource "aws_iam_role" "node_role" {
         Action    = "sts:AssumeRole"
         Effect    = "Allow"
         Principal = {
-          Service = [
-            "ec2.amazonaws.com",
-            "eks.amazonaws.com"
-          ]
+          Service = "ec2.amazonaws.com"
         }
-        Action = "sts:AssumeRole"
       }
     ]
   })
 
   tags = {
-    Name = "eks-role"
+    Name = "eks-node-role"
   }
 }
 
@@ -162,3 +136,28 @@ resource "aws_iam_role_policy_attachment" "ec2_policy" {
   role       = aws_iam_role.node_role.name
   policy_arn  = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
+
+
+# Создание группы узлов (Node Group) для EKS
+resource "aws_eks_node_group" "k8s_node_group" {
+  cluster_name    = aws_eks_cluster.k8s_cluster.name
+  node_role_arn   = aws_iam_role.node_role.arn
+  subnet_ids = [
+    aws_subnet.k8s_subnet_a.id,
+    aws_subnet.k8s_subnet_b.id
+  ]
+  scaling_config {
+    desired_size = var.node_desired_size
+    max_size     = var.node_max_size
+    min_size     = var.node_min_size
+  }
+
+  instance_types = ["t3.medium"] # Укажите тип инстанса EC2 для узлов
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node_policy,
+    aws_iam_role_policy_attachment.cni_policy,
+    aws_iam_role_policy_attachment.ec2_policy,
+  ]
+}
+
