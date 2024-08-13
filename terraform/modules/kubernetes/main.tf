99c012248fad98d53358460e3a8ea4a0f935fc91
boralekc <boralekc@gmail.com>
2024-08-13 17:24:34 +0200

    3

--- terraform/modules/kubernetes/main.tf
+++ terraform/modules/kubernetes/main.tf
@@ -70,46 +70,38 @@ resource "aws_eks_cluster" "k8s_cluster" {
   }
 }
 
-# Создание роли IAM для EKS
+# Роль IAM для EKS
 resource "aws_iam_role" "eks_role" {
   name = "eks-cluster-role"
 
   assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = [
-            "ec2.amazonaws.com",
-            "eks.amazonaws.com"
-          ]
-      }
-      Action = "sts:AssumeRole"
-    }]
     Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
   })
 }
 
-# Политика IAM для роли EKS
-resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.eks_role.name
+resource "aws_iam_role_policy_attachment" "eks_service_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role      = aws_iam_role.eks_role.name
 }
 
-resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.eks_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.eks_role.name
+resource "aws_iam_role_policy_attachment" "eks_service_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role      = aws_iam_role.eks_role.name
 }
 
 # Создание группы узлов (Node Group) для EKS
 resource "aws_eks_node_group" "k8s_node_group" {
   cluster_name    = aws_eks_cluster.k8s_cluster.name
-  node_role_arn   = aws_iam_role.eks_role.arn
+  node_role_arn   = aws_iam_role.node_role.arn
   subnet_ids = [
       aws_subnet.k8s_subnet_a.id,
       aws_subnet.k8s_subnet_b.id
@@ -127,4 +119,46 @@ resource "aws_eks_node_group" "k8s_node_group" {
     aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
     aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
   ]
+}
+
+# Создание роли IAM для узлов EKS
+resource "aws_iam_role" "node_role" {
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = [
+            "ec2.amazonaws.com",
+            "eks.amazonaws.com"
+          ]
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "eks-role"
+  }
+}
+
+# Политики IAM для роли узлов
+resource "aws_iam_role_policy_attachment" "node_policy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn  = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "cni_policy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn  = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_policy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn  = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
