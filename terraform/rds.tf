fd66f678a0cd2c2a59288f03637ed9c5a34c7a61
boralekc <boralekc@gmail.com>
2024-08-21 21:57:19 +0200

    7

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -54,8 +54,8 @@ module "db" {
   # DB subnet group
   create_db_subnet_group = true
   subnet_ids             = [
-    module.rds-vpc.public_subnets,
-    module.rds-vpc.private_subnets
+    module.rds-vpc.public_subnets[*].id,
+    module.rds-vpc.private_subnets[*].id
   ]
   
   # DB parameter group
